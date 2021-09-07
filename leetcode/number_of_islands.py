ba17446c96e6201cfc925970d64c20b952512f49
junyoung <jy940608@naver.com>
2021-09-08 00:43:17 +0900

    210908

---
+++ leetcode/number_of_islands.py
@@ -0,0 +1,24 @@
+"""
+리트코드 200번 문제
+"""
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(i, j):
+            # 더 이상 땅이 아닌 경우
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
+                return
+            # 탐색 한 것은 다른 값으로 바꿈
+            grid[i][j] = '#'
+            
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
+            
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    count += 1
+        return count
