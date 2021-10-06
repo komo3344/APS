9c405ea42c2ca8fd4dda6753c25985b93b22682e
junyoung <jy940608@naver.com>
2021-10-06 23:55:59 +0900

    leetcode 332번 첫번째 풀이

--- leetcode/일정재구성.py
+++ leetcode/일정재구성.py
@@ -1,1 +1,40 @@
 """리트코드 332번 문제"""
+from typing import List
+import collections
+
+tickets = [["JFK", "SFO"], ["JFK", "ATL"], ["SFO", "ATL"], ["ATL", "JFK"], ["ATL", "SFO"]]
+
+
+def findItinerary(tickets: List[List[str]]) -> List[str]:
+    # 그래프 구성
+    graph = collections.defaultdict(list)
+
+    # 1)
+    # for a, b in tickets:
+    #     graph[a].append(b)
+    # for a in graph:
+    #     graph[a].sort()
+
+    # 2)
+    for a, b in sorted(tickets):
+        print(a, b)
+        graph[a].append(b)
+    print("그래프: ", graph)
+    route = []
+
+    def dfs(a):
+        # 첫 번째 값을 읽어 어휘 순 방문
+        while graph[a]:
+            dfs(graph[a].pop(0))
+        route.append(a)
+
+    dfs('JFK')
+    print("결과: ", route)
+    # 다시 뒤집어 어휘 순 결과로
+    return route[::-1]
+"""
+ATL JFK
+"""
+
+answer = findItinerary(tickets)
+print(answer)
